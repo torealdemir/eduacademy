ae9cf45304a0e3acc8c0f94a5f7f6d62571a3b4b
Tore Aldemir <aldemirtore@gmail.com>
2025-11-17 14:05:05 +0300

    add:gh-pages settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// repo adını buraya yaz: /patikaacademy/
 export default defineConfig({
   plugins: [react()],
-  base: '/eduacademy/',
+  base: '/eduacademy/',   // BUNU EKLE
 })
